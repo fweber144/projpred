98fa840e9b56a6173c31fe2b4ab99e916cd59d7d
fweber144 <fweber144@protonmail.com>
2021-04-12 16:01:36 +0200

    Fix a comment.

--- R/refmodel.R
+++ R/refmodel.R
@@ -358,14 +358,15 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
       )
 
       if (length(dim(linpred_out)) == 3) {
-        # In this case, `linpred_out` is expected to be a 3-dimensional array
-        # with dimensions S x N x \tilde{K}, corresponding to posterior draws,
-        # observations (of the original dataset), and outcome categories (or
-        # their latent variants). Therefore, it is coerced to an augmented-rows
-        # matrix with attribute `nobs_orig` which allows to convert it back to a
-        # 3-dimensional array with dimensions N x \tilde{K} x S (by the help of
-        # projpred:::augmat2arr()). For the definition of an "augmented-rows
-        # matrix", see object `mu` further below.
+        # For the augmented-data projection, `linpred_out` is expected to be a
+        # 3-dimensional array with dimensions S x N x \tilde{K}, corresponding
+        # to posterior draws, observations (of the original dataset), and
+        # outcome categories (or their latent variants). Therefore, it is
+        # coerced to an augmented-rows matrix with attribute `nobs_orig` which
+        # allows to convert it back to a 3-dimensional array with dimensions N x
+        # \tilde{K} x S (by the help of projpred:::augmat2arr()). For the
+        # definition of an "augmented-rows matrix", see object `mu` further
+        # below.
         nobs_orig <- dim(linpred_out)[2]
         linpred_out <- apply(linpred_out, 1, as.vector)
         attr(linpred_out, "nobs_orig") <- nobs_orig
